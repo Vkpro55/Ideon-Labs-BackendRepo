13727687b893f7f53810881a9145481574c06956
Vinod Kumar <83464767+Vkpro55@users.noreply.github.com>
2025-01-05 16:36:07 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { config } = require("dotenv")
-const { db, auth } = require('./config/firebaseConfig'); // Import your Firebase config
+const cors = require('cors');  // Import the CORS middleware
+const { config } = require("dotenv");
+const { db, auth } = require('./config/firebaseConfig');
 const { validateDate } = require('./middlewares/dateValidator');
 const { fetchAvailableSlots, bookSlotController, cancelSlotController, getOneUserBookings } = require('./controllers/slotController');
 
 config();
 
 const app = express();
+
+// Enable CORS for all origins (you can modify this to allow specific domains in production)
+app.use(cors());  // Add this line to allow cross-origin requests
+
 app.use(express.json());
 
 /*=== Get Updated 30 minutes all slots for particular date in YYYY-MM-DD format. === */
@@ -35,5 +40,3 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-
-
