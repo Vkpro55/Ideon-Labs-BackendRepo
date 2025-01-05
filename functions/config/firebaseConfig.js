f062d6d0b782dd37c965fc73c6cbfb8748be338f
Vinod Kumar <83464767+Vkpro55@users.noreply.github.com>
2025-01-05 16:03:51 +0530

    Update firebaseConfig.js

--- functions/config/firebaseConfig.js
+++ functions/config/firebaseConfig.js
@@ -1,66 +1,24 @@
-// const admin = require('firebase-admin');
-// const fs = require('node:fs');
-// const serviceAccount = JSON.parse(fs.readFileSync('./firebase-config.json').toString())
-// const serviceAccountString = JSON.stringify(serviceAccount);
-// console.log(serviceAccountString);
-
-// console.log(serviceAccount);
-
-// if (!admin.apps.length) {
-//     try {
-//         admin.initializeApp({
-//             /* ===  credential: admin.credential.applicationDefault() === */
-//             credential: admin.credential.cert(serviceAccount),
-//             /* === databaseURL: process.env.FIREBASE_DATABASE_URL // Add this if needed. === */
-//         });
-//     } catch (error) {
-//         console.error("Firebase Admin SDK initialization error:", error);
-//         throw new Error("Error initializing Firebase Admin SDK");
-//     }
-// } else {
-//     admin.app();
-// }
-
-// /* === Access Firestore === */
-// const db = admin.firestore();
-
-// /* === To define other firebase services like (e.g., Firebase Auth, Firebase Storage) === */
-// const auth = admin.auth();
-
-// module.exports = { db, auth };
-
-
-
-
-var admin = require("firebase-admin");
-var serviceAccount = require("../firebase-config.json");
+const admin = require('firebase-admin');
 
 try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     if (!serviceAccount) {
-        throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable not found.  Check your Render settings.");
+        throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable not set. Check Render settings.");
     }
 
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
+            credential: admin.credential.cert(serviceAccount),
         });
     } else {
         admin.app();
     }
+
     const db = admin.firestore();
     const auth = admin.auth();
     module.exports = { db, auth };
 } catch (error) {
     console.error("Error initializing Firebase:", error);
-    throw error;
+    process.exit(1); 
 }
 
-
-// var admin = require("firebase-admin");
-// var serviceAccount = require("../firebase-config.json");
-
-// console.log(serviceAccount)
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
